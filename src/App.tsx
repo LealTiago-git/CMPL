c3d3c63ae7fa8ac33318479cca85f37da2a5c705
Tiago Leal <tiagoleal03@gmail.com>
2024-09-30 01:30:15 -0300

    1st commit

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <header>
+      fase de desenvolvimento CMPL
+    </header>
+    <a className='text-blue-500' href="https://forms.gle/YDgzFs11wLhzTaxb7">link do forms</a>
+    <div>
+      OBJETIVOS<br />
+      fazer um two page aplication <br />
+      para que o usuario possa ver todos os albuns <br /> 
+      recomendados, assim como, o forms da semana. <br/>
+      Administradores, terão sua propria página<br />
+      com poder de fornecer ao banco de dados <br />
+      os albuns, suas notas, quem recomendou e o link do forms
+      <br />
+      <br />
+      desenvolvimento focado na experiência do celular primeiro
+    </div>
+    <div>
+      <iframe src="https://open.spotify.com/embed/album/49MNmJhZQewjt06rpwp6QR?utm_source=generator" width="100%" height="152"/>
+
+      <iframe src="https://open.spotify.com/embed/album/1CtTTpKbHU8KbHRB4LmBbv?utm_source=generator" width="100%" height="152"/>
+
+      <iframe src="https://open.spotify.com/embed/album/4S5MLu2PKU7uElm2V3JphK?utm_source=generator" width="100%" height="152"/>
+
+      <iframe src="https://open.spotify.com/embed/album/2PfGKHtqEX58bHtkQxJnWG?utm_source=generator" width="100%" height="152"/>
+
+      <iframe src="https://open.spotify.com/embed/album/3eBrvZnagmIDbdgse7cjI6?utm_source=generator" width="100%" height="152"/>
+
+      <iframe src="https://open.spotify.com/embed/album/0W6hOSkA2g1BAXvwHUw7ds?utm_source=generator" width="100%" height="152"/>
+
+      <iframe src="https://open.spotify.com/embed/album/5fmIolILp5NAtNYiRPjhzA?utm_source=generator" width="100%" height="152"/>
+
+      <iframe src="https://open.spotify.com/embed/album/6tkjU4Umpo79wwkgPMV3nZ?utm_source=generator" width="100%" height="152"/>
+
+      <iframe src="https://open.spotify.com/embed/album/30BFY5VHii4PyWqkTubUWX?utm_source=generator" width="100%" height="152"/>
+
+      <iframe src="https://open.spotify.com/embed/album/12Biup1Sq5hpHsX85gJPb2?utm_source=generator" width="100%" height="152"/>
+      </div></>
+  )
+}
+
+export default App
