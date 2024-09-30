50dee4f965f4f35d70ab7b6f0c7e36879860f2f6
Tiago Leal <tiagoleal03@gmail.com>
2024-09-30 17:10:50 -0300

    3rd commit - creating a card for the albums frame

--- src/App.tsx
+++ src/App.tsx
@@ -3,112 +3,180 @@ import './App.css'
 function App() {
     return (
         <>
-            <a
-                className="text-blue-500"
-                href="https://forms.gle/YDgzFs11wLhzTaxb7"
-            >
-                link do forms
-            </a>
-            <div>
+            <div className="space-y-2">
+                <b>Forms</b>
                 <div>
+                    <a
+                        target="_blank"
+                        type="button"
+                        className="flex w-full items-center justify-center rounded-md p-3 font-semibold tracking-wide dark:bg-blue-600 dark:text-gray-50"
+                        href=""
+                    >
+                        aguardando forms <br />
+                        Mezzanine - Massive Attack
+                    </a>
+                    <br />
+                    <a
+                        target="_blank"
+                        type="button"
+                        className="flex w-full items-center justify-center rounded-md p-3 font-semibold tracking-wide dark:bg-blue-600 dark:text-gray-50"
+                        href=""
+                    >
+                        aguardando forms <br />
+                        Mutter - Rammstein
+                    </a>
+                </div>
+            </div>
+            <div className="m-2 space-y-4">
+                <b>Álbuns</b>
+                <div className="space-y-2 rounded-md shadow-xl dark:bg-zinc-800">
                     <iframe
                         src="https://open.spotify.com/embed/album/49MNmJhZQewjt06rpwp6QR?utm_source=generator"
                         width="100%"
                         height="152"
                     />
-                    <a href="">post do insta</a>
+                    <a
+                        target="_blank"
+                        type="button"
+                        className="flex w-full items-center justify-center rounded-md p-3 font-semibold tracking-wide dark:bg-zinc-700 dark:text-gray-50"
+                        href="https://google.com"
+                    >
+                        Aguardo da Avaliação
+                    </a>
                 </div>
-
-                <div>
+                <div className="space-y-2 rounded-md shadow-xl dark:bg-zinc-800">
                     <iframe
                         src="https://open.spotify.com/embed/album/1CtTTpKbHU8KbHRB4LmBbv?utm_source=generator"
                         width="100%"
                         height="152"
                     />
-                    <a href="">post do insta</a>
+                    <a
+                        target="_blank"
+                        type="button"
+                        className="flex w-full items-center justify-center rounded-md p-3 font-semibold tracking-wide dark:bg-zinc-700 dark:text-gray-50"
+                        href=""
+                    >
+                        Aguardo da Avaliação
+                    </a>
                 </div>
-
-                <div>
+                <div className="space-y-2 rounded-md shadow-xl dark:bg-zinc-800">
                     <iframe
                         src="https://open.spotify.com/embed/album/4S5MLu2PKU7uElm2V3JphK?utm_source=generator"
                         width="100%"
                         height="152"
                     />
-                    <a href="">post do insta</a>
+                    <a
+                        target="_blank"
+                        type="button"
+                        className="flex w-full items-center justify-center rounded-md p-3 font-semibold tracking-wide dark:bg-zinc-700 dark:text-gray-50"
+                        href=""
+                    >
+                        Aguardo da Avaliação
+                    </a>
                 </div>
-
-                <div>
+                <div className="space-y-2 rounded-md shadow-xl dark:bg-zinc-800">
                     <iframe
                         src="https://open.spotify.com/embed/album/3eBrvZnagmIDbdgse7cjI6?utm_source=generator"
                         width="100%"
                         height="152"
                     />
-                    <a href="">post do insta</a>
+                    <a
+                        target="_blank"
+                        type="button"
+                        className="flex w-full items-center justify-center rounded-md p-3 font-semibold tracking-wide dark:bg-zinc-700 dark:text-gray-50"
+                        href=""
+                    >
+                        Aguardo da Avaliação
+                    </a>
                 </div>
-
-                <div>
+                <div className="space-y-2 rounded-md shadow-xl dark:bg-zinc-800">
                     <iframe
                         src="https://open.spotify.com/embed/album/5fmIolILp5NAtNYiRPjhzA?utm_source=generator"
                         width="100%"
                         height="152"
                     />
-                    <a href="https://www.instagram.com/p/DASGrPSuY3M/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==">
-                        post do insta
+                    <a
+                        target="_blank"
+                        type="button"
+                        className="flex w-full items-center justify-center rounded-md p-3 font-semibold tracking-wide dark:bg-zinc-700 dark:text-gray-50"
+                        href="https://www.instagram.com/p/DASGrPSuY3M/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+                    >
+                        Avaliação
                     </a>
                 </div>
-
-                <div>
+                <div className="space-y-2 rounded-md shadow-xl dark:bg-zinc-800">
                     <iframe
                         src="https://open.spotify.com/embed/album/0W6hOSkA2g1BAXvwHUw7ds?utm_source=generator"
                         width="100%"
                         height="152"
                     />
-                    <a href="https://www.instagram.com/p/DASFfkvOOsp/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==">
-                        post do insta
+                    <a
+                        target="_blank"
+                        type="button"
+                        className="flex w-full items-center justify-center rounded-md p-3 font-semibold tracking-wide dark:bg-zinc-700 dark:text-gray-50"
+                        href="https://www.instagram.com/p/DASFfkvOOsp/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+                    >
+                        Avaliação
                     </a>
                 </div>
-
-                <div>
+                <div className="space-y-2 rounded-md shadow-xl dark:bg-zinc-800">
                     <iframe
                         src="https://open.spotify.com/embed/album/6tkjU4Umpo79wwkgPMV3nZ?utm_source=generator"
                         width="100%"
                         height="152"
                     />
-                    <a href="https://www.instagram.com/p/C_9dg_Sud8h/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==">
-                        post do insta
+                    <a
+                        target="_blank"
+                        type="button"
+                        className="flex w-full items-center justify-center rounded-md p-3 font-semibold tracking-wide dark:bg-zinc-700 dark:text-gray-50"
+                        href="https://www.instagram.com/p/C_9dg_Sud8h/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+                    >
+                        Avaliação
                     </a>
                 </div>
-
-                <div>
+                <div className="space-y-2 rounded-md shadow-xl dark:bg-zinc-800">
                     <iframe
                         src="https://open.spotify.com/embed/album/2PfGKHtqEX58bHtkQxJnWG?utm_source=generator"
                         width="100%"
                         height="152"
                     />
-                    <a href="https://www.instagram.com/p/C_9cr1FOvPD/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==">
-                        post do insta
+                    <a
+                        target="_blank"
+                        type="button"
+                        className="flex w-full items-center justify-center rounded-md p-3 font-semibold tracking-wide dark:bg-zinc-700 dark:text-gray-50"
+                        href="https://www.instagram.com/p/C_9cr1FOvPD/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+                    >
+                        Avaliação
                     </a>
                 </div>
-
-                <div>
+                <div className="space-y-2 rounded-md shadow-xl dark:bg-zinc-800">
                     <iframe
                         src="https://open.spotify.com/embed/album/30BFY5VHii4PyWqkTubUWX?utm_source=generator"
                         width="100%"
                         height="152"
                     />
-                    <a href="https://www.instagram.com/p/C_suaNLOgr2/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==">
-                        post do insta
+                    <a
+                        target="_blank"
+                        type="button"
+                        className="flex w-full items-center justify-center rounded-md p-3 font-semibold tracking-wide dark:bg-zinc-700 dark:text-gray-50"
+                        href="https://www.instagram.com/p/C_suaNLOgr2/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+                    >
+                        Avaliação
                     </a>
                 </div>
-
-                <div>
+                <div className="space-y-2 rounded-md shadow-xl dark:bg-zinc-800">
                     <iframe
                         src="https://open.spotify.com/embed/album/12Biup1Sq5hpHsX85gJPb2?utm_source=generator"
                         width="100%"
                         height="152"
                     />
-                    <a href="https://www.instagram.com/p/C_suEZdOTpM/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==">
-                        post do insta
+                    <a
+                        target="_blank"
+                        type="button"
+                        className="flex w-full items-center justify-center rounded-md p-3 font-semibold tracking-wide dark:bg-zinc-700 dark:text-gray-50"
+                        href="https://www.instagram.com/p/C_suEZdOTpM/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+                    >
+                        Avaliação
                     </a>
                 </div>
             </div>
