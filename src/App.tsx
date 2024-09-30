94e32ddafcaf6db45ec06145c66b11cad69a7eb4
Tiago Leal <tiagoleal03@gmail.com>
2024-09-30 12:21:38 -0300

    Clearing the face of the site

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,119 @@
 import './App.css'
 
 function App() {
-  return (
-    <>
-    <header>
-      fase de desenvolvimento CMPL
-    </header>
-    <a className='text-blue-500' href="https://forms.gle/YDgzFs11wLhzTaxb7">link do forms</a>
-    <div>
-      OBJETIVOS<br />
-      fazer um two page aplication <br />
-      para que o usuario possa ver todos os albuns <br /> 
-      recomendados, assim como, o forms da semana. <br/>
-      Administradores, terão sua propria página<br />
-      com poder de fornecer ao banco de dados <br />
-      os albuns, suas notas, quem recomendou e o link do forms
-      <br />
-      <br />
-      desenvolvimento focado na experiência do celular primeiro
-    </div>
-    <div>
-      <iframe src="https://open.spotify.com/embed/album/49MNmJhZQewjt06rpwp6QR?utm_source=generator" width="100%" height="152"/>
+    return (
+        <>
+            <a
+                className="text-blue-500"
+                href="https://forms.gle/YDgzFs11wLhzTaxb7"
+            >
+                link do forms
+            </a>
+            <div>
+                <div>
+                    <iframe
+                        src="https://open.spotify.com/embed/album/49MNmJhZQewjt06rpwp6QR?utm_source=generator"
+                        width="100%"
+                        height="152"
+                    />
+                    <a href="">post do insta</a>
+                </div>
 
-      <iframe src="https://open.spotify.com/embed/album/1CtTTpKbHU8KbHRB4LmBbv?utm_source=generator" width="100%" height="152"/>
+                <div>
+                    <iframe
+                        src="https://open.spotify.com/embed/album/1CtTTpKbHU8KbHRB4LmBbv?utm_source=generator"
+                        width="100%"
+                        height="152"
+                    />
+                    <a href="">post do insta</a>
+                </div>
 
-      <iframe src="https://open.spotify.com/embed/album/4S5MLu2PKU7uElm2V3JphK?utm_source=generator" width="100%" height="152"/>
+                <div>
+                    <iframe
+                        src="https://open.spotify.com/embed/album/4S5MLu2PKU7uElm2V3JphK?utm_source=generator"
+                        width="100%"
+                        height="152"
+                    />
+                    <a href="">post do insta</a>
+                </div>
 
-      <iframe src="https://open.spotify.com/embed/album/2PfGKHtqEX58bHtkQxJnWG?utm_source=generator" width="100%" height="152"/>
+                <div>
+                    <iframe
+                        src="https://open.spotify.com/embed/album/3eBrvZnagmIDbdgse7cjI6?utm_source=generator"
+                        width="100%"
+                        height="152"
+                    />
+                    <a href="">post do insta</a>
+                </div>
 
-      <iframe src="https://open.spotify.com/embed/album/3eBrvZnagmIDbdgse7cjI6?utm_source=generator" width="100%" height="152"/>
+                <div>
+                    <iframe
+                        src="https://open.spotify.com/embed/album/5fmIolILp5NAtNYiRPjhzA?utm_source=generator"
+                        width="100%"
+                        height="152"
+                    />
+                    <a href="https://www.instagram.com/p/DASGrPSuY3M/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==">
+                        post do insta
+                    </a>
+                </div>
 
-      <iframe src="https://open.spotify.com/embed/album/0W6hOSkA2g1BAXvwHUw7ds?utm_source=generator" width="100%" height="152"/>
+                <div>
+                    <iframe
+                        src="https://open.spotify.com/embed/album/0W6hOSkA2g1BAXvwHUw7ds?utm_source=generator"
+                        width="100%"
+                        height="152"
+                    />
+                    <a href="https://www.instagram.com/p/DASFfkvOOsp/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==">
+                        post do insta
+                    </a>
+                </div>
 
-      <iframe src="https://open.spotify.com/embed/album/5fmIolILp5NAtNYiRPjhzA?utm_source=generator" width="100%" height="152"/>
+                <div>
+                    <iframe
+                        src="https://open.spotify.com/embed/album/6tkjU4Umpo79wwkgPMV3nZ?utm_source=generator"
+                        width="100%"
+                        height="152"
+                    />
+                    <a href="https://www.instagram.com/p/C_9dg_Sud8h/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==">
+                        post do insta
+                    </a>
+                </div>
 
-      <iframe src="https://open.spotify.com/embed/album/6tkjU4Umpo79wwkgPMV3nZ?utm_source=generator" width="100%" height="152"/>
+                <div>
+                    <iframe
+                        src="https://open.spotify.com/embed/album/2PfGKHtqEX58bHtkQxJnWG?utm_source=generator"
+                        width="100%"
+                        height="152"
+                    />
+                    <a href="https://www.instagram.com/p/C_9cr1FOvPD/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==">
+                        post do insta
+                    </a>
+                </div>
 
-      <iframe src="https://open.spotify.com/embed/album/30BFY5VHii4PyWqkTubUWX?utm_source=generator" width="100%" height="152"/>
+                <div>
+                    <iframe
+                        src="https://open.spotify.com/embed/album/30BFY5VHii4PyWqkTubUWX?utm_source=generator"
+                        width="100%"
+                        height="152"
+                    />
+                    <a href="https://www.instagram.com/p/C_suaNLOgr2/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==">
+                        post do insta
+                    </a>
+                </div>
 
-      <iframe src="https://open.spotify.com/embed/album/12Biup1Sq5hpHsX85gJPb2?utm_source=generator" width="100%" height="152"/>
-      </div></>
-  )
+                <div>
+                    <iframe
+                        src="https://open.spotify.com/embed/album/12Biup1Sq5hpHsX85gJPb2?utm_source=generator"
+                        width="100%"
+                        height="152"
+                    />
+                    <a href="https://www.instagram.com/p/C_suEZdOTpM/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==">
+                        post do insta
+                    </a>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App
---
+++ src/Dev.tsx
@@ -0,0 +1,23 @@
+import './Dev.css'
+
+export default function Dev() {
+    return (
+        <div>
+            <header>fase de desenvolvimento CMPL</header>
+            <div>
+                OBJETIVOS
+                <br />
+                fazer um two page aplication <br />
+                para que o usuario possa ver todos os albuns <br />
+                recomendados, assim como, o forms da semana. <br />
+                Administradores, terão sua propria página
+                <br />
+                com poder de fornecer ao banco de dados <br />
+                os albuns, suas notas, quem recomendou e o link do forms
+                <br />
+                <br />
+                desenvolvimento focado na experiência do celular primeiro
+            </div>
+        </div>
+    )
+}
